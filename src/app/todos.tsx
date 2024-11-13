82f19563e947229a12430c0611158c1dc3e2fba7
Sam Selikoff <sam.selikoff@gmail.com>
2024-11-12 21:08:16 -0500

    wip

--- src/app/todos.tsx
+++ src/app/todos.tsx
@@ -12,30 +12,30 @@ export default function Todos({
   const [draftTodos, action, isPending] = useActionState(
     async (
       clientTodos: { id?: string; text: string; clientId: string }[],
-      props:
+      {
+        type,
+        payload,
+      }:
         | { type: "create"; payload: { clientId: string } }
         | {
             type: "update";
             payload: { clientId: string; text: string };
           },
     ) => {
-      switch (props.type) {
+      switch (type) {
         case "create":
           const newTodo = await createTodo();
-          return [
-            ...clientTodos,
-            { clientId: props.payload.clientId, ...newTodo },
-          ];
+          return [...clientTodos, { clientId: payload.clientId, ...newTodo }];
 
         case "update":
           const id = clientTodos.find(
-            (t) => t.clientId === props.payload.clientId,
+            (t) => t.clientId === payload.clientId,
           )?.id;
           if (!id) return clientTodos;
 
           const updatedTodo = await updateTodo({
             id,
-            text: props.payload.text,
+            text: payload.text,
           });
           return clientTodos.map((t) =>
             t.id === updatedTodo.id ? { ...t, ...updatedTodo } : t,
@@ -44,35 +44,6 @@ export default function Todos({
         default:
           return clientTodos;
       }
-
-      // console.log(previous);
-      // switch (props.formData.get("_action")) {
-      //   case "create":
-      //     const newTodo = await createTodo();
-      //     return [...previous, { ...newTodo, clientId: props.clientId }];
-
-      //   case "update":
-      //     const text = props.formData.get("text");
-
-      //     if (typeof text !== "string") {
-      //       return previous;
-      //     }
-
-      //     const id = previous.find((t) => t.clientId === props.clientId)?.id;
-      //     if (!id) {
-      //       return previous;
-      //     }
-
-      //     const updatedTodo = await updateTodo({ id, text });
-      //     return previous.map((t) =>
-      //       t.id === updatedTodo.id
-      //         ? { ...updatedTodo, clientId: props.clientId }
-      //         : t,
-      //     );
-
-      //   default:
-      //     return previous;
-      // }
     },
     todos.map((t) => ({ ...t, clientId: t.id })),
   );
@@ -121,7 +92,6 @@ export default function Todos({
                 placeholder="Text..."
                 defaultValue={todo.text}
                 name="text"
-                type="text"
               />
             </form>
           </div>
